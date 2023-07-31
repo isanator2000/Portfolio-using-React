7bcd95b6f1defb463bce70a7377da631b2063b5d
Isa <iruizord@gmail.com>
2023-07-31 11:02:21 -0600

    added content to the about page and styled it

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import '../styling/about.css'
 
 function About () {
     return ( 
-    <div>About</div> 
+    <div className='aboutMe'>
+        <div className='aboutIntro'> 
+        <h3>About Me</h3>
+        <p>This text is temporary, as I'm only writing this as a placeholder while I actually come up with something interesting.
+            I put a lot of effort into making this assignment! I know it's not that pretty but I'm kinda proud of myself.
+            I'll come back to it later to make improvements and turn it into a real, more professional portfolio as I'm sure it'll be of use.
+            Thanks for visiting anyway! 
+        </p>
+        </div>
+    </div> 
     )
 }
 
