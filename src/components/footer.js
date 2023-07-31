4742dcb793e6fa543e9dc8d666670615cebd5f81
Isa <iruizord@gmail.com>
2023-07-31 00:40:52 -0600

    styling footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import GithubIcon from '@material-ui/icons/GitHub';
+import '../styling/footer.css';
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+        <p>Thank you for visiting my portfolio page! It's still a WIP so come visit again later!</p>
+        <div className='githubIcon'> <GithubIcon /></div>
+    </div>
   )
 }
 
