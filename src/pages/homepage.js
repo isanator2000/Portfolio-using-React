8b8d33e40e8f9512037d196b95fce41579933307
Isa <iruizord@gmail.com>
2023-07-31 01:18:01 -0600

    adding content to the homepage

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -2,7 +2,26 @@ import React from 'react'
 
 function Home () {
     return ( 
-    <div>Home</div> 
+    <div className='homepage'>
+        <div className='intro'>
+            <h1>I'm Isa.</h1>
+            <div className='phrase'><p>I'm an industrial designer trying to learn how to code.</p></div>
+        </div>
+
+        <div className='skills'>
+            <h1>Some of the skills I'm currently working on are...</h1>
+                <ol className='skillList'> 
+                    <li className='skill'>
+                        <h3>Design and Product Development</h3>
+                        <span>3D modeling, rendering, UX/UI</span>
+                    </li>
+                    <li className='skill'>
+                        <h3>Coding (Front-end)</h3>
+                        <span>HTML, CSS, JavaScript, React</span>
+                    </li>
+                </ol>
+        </div>
+    </div> 
     )
 }
 
