5106a16d0ed0ee4d308b0f1156b847938945969e
Isa <iruizord@gmail.com>
2023-07-31 10:08:33 -0600

    added works page content and styling, and edited homepage styling for consistency

--- src/pages/works.js
+++ src/pages/works.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import '../styling/works.css';
+
 
 function Works () {
     return ( 
-    <div>Works</div> 
+    <div>
+        <div className='projects'>
+            <h1>Some of the things I've done so far are...</h1>
+                <ol className='projectList'> 
+                    <div className='projectColumn'>
+                    <li className='project'>
+                        <h3>Moodify</h3>
+                    </li>
+                    <li className='project'>
+                        <h3>BetBud</h3>
+                    </li>
+                    </div>
+                    <div className='projectColumn'>
+                    <li className='project'>
+                        <h3>JavaScript Quiz</h3>
+                    </li>
+                    <li className='project'>
+                        <h3>JavaScript Password Generator</h3>
+                    </li>
+                    </div>
+                </ol>
+        </div>
+    </div> 
     )
 }
 
