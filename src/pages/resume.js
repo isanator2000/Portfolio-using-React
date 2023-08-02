399c33f0856c9177433f19c70d7afdc1781a5d1e
Isa <iruizord@gmail.com>
2023-08-01 21:10:53 -0600

    added my resume and styled that page

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import '../styling/resume.css'
+
+
 
 function Resume() {
   return (
-    <div>Resume</div>
-  )
+    <div className='resume-container'>
+      <div className='resume-box'>
+        Download my resume <a href='https://drive.google.com/file/d/1PmIdAG-RHJ4ZM8_qDZkttrTzytl1Llkq/view?usp=sharing'>here</a>!
+      </div>
+    </div>
+  );
 }
 
 export default Resume
