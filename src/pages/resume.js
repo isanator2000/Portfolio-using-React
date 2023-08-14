8538bc1d9351f8b4d1b3e293733dd0abc7f1cd21
Isa <iruizord@gmail.com>
2023-08-13 19:07:49 -0600

    added text

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -6,7 +6,7 @@ function Resume() {
   return (
     <div className='resume-container'>
       <div className='resume-box'>
-        Download my resume <a href='https://drive.google.com/file/d/1PmIdAG-RHJ4ZM8_qDZkttrTzytl1Llkq/view?usp=sharing'>here</a>!
+        Download my resume over<a href='https://drive.google.com/file/d/1PmIdAG-RHJ4ZM8_qDZkttrTzytl1Llkq/view?usp=sharing'>here</a>!
       </div>
     </div>
   );
